cd671a5d66234265a6a92a739feb968c360a9d54
Sanjeev Kumar <sanjeevkumar@deloitte.com>
2022-10-05 20:17:54 +0530

    web creating components

--- Greet.tsx
+++ Greet.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
-import Hello from './component/Hello';
-import Greet from './Greet';
-const App = () => {
+const Greet = () => {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
       <View style={{alignItems: 'center'}}>
-        <Text style={{fontSize: 24}}>Sanjeev</Text>
-      </View>
-      <View>
-        <Greet />
-        <Hello />
+        <Text style={{fontSize: 20}}>Greet</Text>
       </View>
     </SafeAreaView>
   );
 };
-export default App;
+export default Greet;
---
+++ component/Hello.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const Hello = () => {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text style={{fontSize: 15}}>Hello</Text>
+    </View>
+  );
+};
+export default Hello;
